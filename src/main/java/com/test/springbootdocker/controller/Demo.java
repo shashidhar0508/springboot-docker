6a6c6785ef506bf2e17ab4e734be1c067333de2a
shashidhar0508 <shashidhar0508@gmail.com>
2021-11-24 07:01:48 +0530

    Added logback file for logging

--- src/main/java/com/test/springbootdocker/controller/Demo.java
+++ src/main/java/com/test/springbootdocker/controller/Demo.java
@@ -1,13 +1,29 @@
 package com.test.springbootdocker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Demo {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/getMessage")
     public String getMessage(){
+        int count =1;
+        for(int i=0;i<10;i++) {
+            logger.info("count value : "+count);
+            count++;
+        }
+        return "shashidhar";
+    }
+
+    @GetMapping("/getError")
+    public String getError(){
+        int x = 10/0;
+        logger.info("x value : "+x);
         return "shashidhar";
     }
 }
